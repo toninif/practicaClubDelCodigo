3007728537f091f9b5ee7a9e9c95cb8410f2e9be
Fernando <fernandotonini11@gmail.com>
2022-12-06 10:57:03 -0300

    agregue carpetas

---
+++ funciones/carga_de_paquetes.R
@@ -0,0 +1,20 @@
+#
+#
+# Funcion para instalar paquetes en caso de no tenerlos
+#
+# 
+
+# Primero se crea un objeto que contenga los paquetes que utilizaremos
+# Esto se hace nombrando al objeto (en este caso paquetes_usados), luego
+# utilizando la flecha de asignacion ( <- ) y luego el conjunto de paquetes
+# con la funcion c()
+
+paquetes_usados <- c("tidyverse", "ggdog")
+
+
+installed_packages <- paquetes_usados %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) { # Esta funcion hace el siguiente checkeo: si (if) alguno (any) de los paquetes
+                                        # en "installed_packages" no esta presente (es decir devuelve FALSE)
+                                        # entonces instala esos paquetes
+  install.packages(paquetes_usados[!installed_packages])
+}
